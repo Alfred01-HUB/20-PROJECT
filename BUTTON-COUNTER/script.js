1f2aaca8ea6c55361f7e7eafa36e979991cc8019
Alfred01-HUB <alfredolabanji066@gmail.com>
2026-02-03 16:48:32 +0100

    creating a button counter logic

---
+++ BUTTON-COUNTER/script.js
@@ -0,0 +1,13 @@
+const countDisplay = document.getElementById('count')
+const button = document.getElementById('clickButton')
+console.log(button)
+
+let count = 0
+
+function incrementCount(){
+    count ++
+    countDisplay.textContent=count;
+}
+
+button.addEventListener('click',incrementCount)
+console.log(countDisplay,button)
